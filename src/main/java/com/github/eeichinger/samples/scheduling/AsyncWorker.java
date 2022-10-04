f13f1363243d36ca33392a3f8c43f39ccb1f92bd
Erich Eichinger <eeichinger@gmail.com>
2022-10-04 21:29:37 +0200

    use apache clone() instead of own method

--- src/main/java/com/github/eeichinger/samples/scheduling/AsyncWorker.java
+++ src/main/java/com/github/eeichinger/samples/scheduling/AsyncWorker.java
@@ -1,13 +1,10 @@
 package com.github.eeichinger.samples.scheduling;
 
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.SerializationUtils;
 import org.springframework.security.concurrent.DelegatingSecurityContextRunnable;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-
-import java.io.*;
 
 /**
  * This class captures the (security) context of the thread that creates an instance (see constructor).
@@ -22,11 +19,10 @@ public class AsyncWorker implements Runnable {
     final String currentTenantId;
     final Runnable task;
 
-    @SneakyThrows
     public AsyncWorker(TenantService tenantService, Runnable task) {
         this.tenantService = tenantService;
         this.currentTenantId = tenantService.getCurrentTenantId(); // capture tenant from current thread
-        SecurityContext securityContext = cloneObject(SecurityContextHolder.getContext(), SecurityContext.class);
+        SecurityContext securityContext = SerializationUtils.clone(SecurityContextHolder.getContext());
         // override only for logging
         this.task = new DelegatingSecurityContextRunnable(() -> {
             log.info("executing important process on behalf of tenant {} and user '{}/'", currentTenantId, securityContext.getAuthentication());
@@ -37,33 +33,7 @@ public class AsyncWorker implements Runnable {
         log.info("created new job instance for user {}", securityContext.getAuthentication());
     }
 
-    private static <T> T cloneObject(T object, Class<T> clazz) {
-        byte[] data = serializeObject(object);
-        return deserializeObject(data, clazz);
-    }
-
-    @SuppressWarnings("unchecked")
-    @SneakyThrows
-    private static <T> T deserializeObject(byte[] data, Class<T> clazz) {
-        ByteArrayInputStream bais = new ByteArrayInputStream(data);
-        ObjectInputStream objectInputStream = new ObjectInputStream(bais);
-        Object o = objectInputStream.readObject();
-        objectInputStream.close();
-        return (T)o;
-    }
-
-    @SneakyThrows
-    private static byte[] serializeObject(Object securityContext) {
-        ByteArrayOutputStream baus = new ByteArrayOutputStream();
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(baus);
-        objectOutputStream.writeObject(securityContext);
-        objectOutputStream.flush();
-        objectOutputStream.close();
-        return baus.toByteArray();
-    }
-
     @Override
-    @SneakyThrows
     public void run() {
         tenantService.runAsTenant(currentTenantId, () -> {
             task.run(); // do the work
